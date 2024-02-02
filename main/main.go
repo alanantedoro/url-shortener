4f53827317d5652a15f082422256bc36efed9125
alanantedoro <alanantedoro@gmail.com>
2024-02-02 10:15:17 -0300

    quick last fix

--- main/main.go
+++ main/main.go
@@ -11,24 +11,22 @@ import (
 	"github.com/boltdb/bolt"
 )
 
-
 func main() {
 	var yamlFile, jsonFile, boltDB string
-	 flag.StringVar(&yamlFile,"yaml","","a yaml file in the format of 'path and url'")
-	flag.StringVar(&jsonFile,"json","","a json file in the format of 'path and url'")
-	flag.StringVar(&boltDB,"bolt","","a json file in the format of 'path and url'")
+	flag.StringVar(&yamlFile, "yaml", "", "a yaml file in the format of 'path and url'")
+	flag.StringVar(&jsonFile, "json", "", "a json file in the format of 'path and url'")
+	flag.StringVar(&boltDB, "bolt", "", "a json file in the format of 'path and url'")
 
 	flag.Parse()
 	mux := defaultMux()
 
-	
 	pathsToUrls := map[string]string{
 		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 	}
 	mapHandler := urlshortener.MapHandler(pathsToUrls, mux)
-	
-	if(yamlFile != ""){
+
+	if yamlFile != "" {
 		yamlContent, err := os.ReadFile(yamlFile)
 		if err != nil {
 			panic(err)
@@ -39,19 +37,19 @@ func main() {
 		}
 		fmt.Println("Starting the server on :8080")
 		http.ListenAndServe(":8080", yamlHandler)
-	} else if jsonFile != ""{
+	} else if jsonFile != "" {
 		jsonContent, err := os.ReadFile(jsonFile)
 		if err != nil {
 			panic(err)
 		}
-		
+
 		jsonHandler, err := urlshortener.JSONHandler(jsonContent, mapHandler)
 		if err != nil {
 			panic(err)
 		}
 		fmt.Println("Starting the server on :8080")
 		http.ListenAndServe(":8080", jsonHandler)
-	} else if(boltDB != ""){
+	} else if boltDB != "" {
 		db, err := bolt.Open(boltDB, 0600, nil)
 		if err != nil {
 			log.Fatal(err)
@@ -63,15 +61,15 @@ func main() {
 		// 	if err != nil {
 		// 		return fmt.Errorf("create bucket: %s", err)
 		// 	}
-		
+
 		// 	if err := b.Put([]byte("/bolt"), []byte("https://pkg.go.dev/github.com/boltdb/bolt")); err != nil {
 		// 		return fmt.Errorf("put data: %s", err)
 		// 	}
-		
+
 		// 	return nil
 		// })
 
-		boltHandler := urlshortener.BOLThanlder(db,mapHandler)
+		boltHandler := urlshortener.BOLThanlder(db, mapHandler)
 		fmt.Println("Starting the server on :8080")
 		http.ListenAndServe(":8080", boltHandler)
 
@@ -89,5 +87,3 @@ func defaultMux() *http.ServeMux {
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, world!")
 }
-
-
